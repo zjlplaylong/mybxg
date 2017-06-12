419a0821647a6bf417598686e05119d4a81bfa0a
zhangjl <75675746@qq.com>
2017-06-12 18:59:18 +0800

    完成添加和编辑功能

--- public/js/teacher-add.js
+++ public/js/teacher-add.js
@@ -3,22 +3,39 @@ define(['jquery','util','template'],function($,util,template){
     util.setMenu('/teacher/list');
     // 获取参数中tc_id
     var tcId = util.getSearch('tc_id',location.search);
-    console.log(tcId)
     // 表单处理
-    $("#addTeacherBtn").click(function() {
-        console.log(1);
-        $.ajax({
-            type : 'post',
-            url : '/api/teacher/add',
-            data : $("#addTeacherForm").serialize(),
-            dataType : 'json',
-            success : function(data){
-                location.href = '/teacher/list';
-            }
+    function submitForm(url){
+        $("#addTeacherBtn").click(function() {
+            $.ajax({
+                type : 'post',
+                url : url,
+                data : $("#addTeacherForm").serialize(),
+                dataType : 'json',
+                success : function(data){
+                    if(data.code == 200){
+                        location.href = '/teacher/list';
+                    }
+                }
+            });
         });
-    });
+    }
+    // $("#addTeacherBtn").click(function() {
+    //     console.log(1);
+    //     $.ajax({
+    //         type : 'post',
+    //         url : '/api/teacher/add',
+    //         data : $("#addTeacherForm").serialize(),
+    //         dataType : 'json',
+    //         success : function(data){
+    //             if(data.code == 200){
+    //                 location.href = '/teacher/list';
+    //             }
+    //         }
+    //     });
+    // });
     // 获取参数中tc_id
-    var tcId = util.getSearch('tc_id',location.search);
+    // var tcId = util.getSearch('tc_id',location.search);
+
     if(tcId){
         //编辑讲师
         $.ajax({
@@ -30,12 +47,14 @@ define(['jquery','util','template'],function($,util,template){
                 data.result.operateFlag = '编辑';
                 var html = template("teacherFormTpl",data.result);
                 $("#teacherFormInfo").html(html);
+                submitForm('/api/teacher/update');
             }
         });
     }else {
         // 添加讲师
         $("#navFlag").html('讲师添加');
-        var html = template("teacherFormTpl",{operateFlag : '添加'});
+        var html = template("teacherFormTpl",{operateFlag : '添加',tc_gender : '1'});
         $("#teacherFormInfo").html(html);
+        submitForm('/api/teacher/add');
     }
 });
